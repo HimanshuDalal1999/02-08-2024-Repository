34131b98f305860531e0b1d05a05b57f4afcf1a9
Himanshu@Dalal <himanshuapachertr160@gmail.com>
2024-08-02 19:22:32 +0530

    Sample commit

---
+++ src/Package/Class.java
@@ -0,0 +1,13 @@
+package Package;
+
+public class Class
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Himanshu");
+		System.out.println("Janardhanji");
+		System.out.println("Dalal");
+		
+	}
+
+}
